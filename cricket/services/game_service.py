12223abd66a761897bf91be0ea400672a5d07d93
C. Parker <Parker.chrisdavid@gmail.com>
2022-05-22 18:09:13 -0700

    Fixed all the lists...Maybe.

--- cricket/services/game_service.py
+++ cricket/services/game_service.py
@@ -15,7 +15,7 @@ def get_games_count() -> int:
         session.close()
 
 
-def find_games_by_username(username: str) -> Optional[list[Game]]:
+def find_games_by_username(username: str) -> Optional[List[Game]]:
     session = db_session.create_session()
     try:
         return list(session.query(Game).filter(Game.player_one_username == username).order_by(Game.id.desc()))
